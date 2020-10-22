ea429dedfd897aa21e5eee6d2b4bfc26e068431c
Vvramen <ramensky.exceedteam.com>
2020-10-22 14:51:26 +0300

    problem solving

--- 2. Data Types/2) sum-check/index.js
+++ 2. Data Types/2) sum-check/index.js
@@ -1,5 +1,8 @@
 function sumCheck(a, b) {
-  return(+a + +b);
+  if (typeof a == "number" && typeof b == "number") {
+    return a + b;
+  }
+  throw new Error("Wrong arguments type!");
 }
 
 window.sumCheck = sumCheck;
