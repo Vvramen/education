467cf9199db4f19af65b17e53bb25daae141a0c5
Vvramen <ramensky.exceedteam.com>
2020-10-22 15:23:05 +0300

    task solved

--- 2. Data Types/3) is-equal-simple/index.js
+++ 2. Data Types/3) is-equal-simple/index.js
@@ -1,5 +1,21 @@
 function isEqual(a, b) {
-  // Напиши свой код здесь
+  if (!a || !b) return false;
+
+  for (let key in a) {
+      if (a.hasOwnProperty(key)) {
+          if (a[key] !== b[key] || !b.hasOwnProperty(key)) {
+              return false;
+          }
+      }
+  }
+  for (let key in b) {
+      if (b.hasOwnProperty(key)) {
+          if (a[key] !== b[key] || !a.hasOwnProperty(key)) {
+              return false;
+          }
+      }
+  }
+  return true;
 }
 
 window.isEqual = isEqual;
