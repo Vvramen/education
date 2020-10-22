9730255694f74763ac92da78314f804a26e34596
Vvramen <ramensky.exceedteam.com>
2020-10-22 17:23:23 +0300

    task solved

--- 3. Array/1) full-sum/index.js
+++ 3. Array/1) full-sum/index.js
@@ -1,7 +1,18 @@
 function fullSum(...args) {
-  // Напиши свой код здесь
+    for (let i = 0; i < args.length; i++) {
+        if (typeof args[i] !== 'number') throw new Error("Wrong Argument Type");
+    };
+
+    let arr = [];
+    for (let i = 0; i < args.length; i++) {
+        arr[i] = args[i];
+    }
+
+    let result = arr.reduce(function(sum, current) {
+        return sum + current;
+    }, 0);
+    return result;
 }
 
 window.fullSum = fullSum;
-
 export default fullSum;
