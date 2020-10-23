fdc8ff04321442a8ce6024f601f146e9a5863d21
Vvramen <ramensky.exceedteam.com>
2020-10-23 10:43:35 +0300

    task solved

--- 3. Array/4) query-object/index.js
+++ 3. Array/4) query-object/index.js
@@ -1,6 +1,14 @@
+import stringToType from "../../2. Data Types/1) string-to-type/index.js";
+
 function queryToObject(query) {
-  // Напиши свой код здесь
-};
+    let f = query.split("?").join("");
+    return f.split("&").reduce((ob, v) => {
+        v = v.split("=");
+        ob[v[0]] = stringToType(v[1]);
+        return ob;
+    }, {});
+    return obj;
+}
 
 window.queryToObject = queryToObject;
 
