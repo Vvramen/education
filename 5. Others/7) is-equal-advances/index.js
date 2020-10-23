e9946972c7be467efb4ea70bf44140cff81d558c
Vvramen <ramensky.exceedteam.com>
2020-10-23 16:50:44 +0300

    task solved

--- 5. Others/7) is-equal-advances/index.js
+++ 5. Others/7) is-equal-advances/index.js
@@ -1,5 +1,30 @@
 function isEqualAdvances(a, b) {
-  // Напиши свой код здесь
+    if (!a && !b) return true;
+    if (a == null || b == null) return false;
+
+    const keysA = Object.keys(a);
+    const keysB = Object.keys(b);
+    if (!keysA.length && !keysB.length) return true;
+    if (keysA.length !== keysB.length) return false;
+
+    let res;
+    let i = 0;
+    for (let key in a) {
+        if (typeof a[key] == "object" && typeof b[key] == "object") {
+            return isEqualAdvances(a[key], b[key]);
+        }
+        if (typeof a[key] == "object" && typeof b[key] !== "object") return false;
+        if (typeof a[key] !== "object" && typeof b[key] == "object") return false;
+        if (
+            String(keysA[i]) == String(keysB[i]) &&
+            String(a[key]) == String(b[key])
+        ) {
+            res = true;
+        } else {
+            res = false;
+        }
+    }
+    return res;
 }
 
 window.isEqualAdvances = isEqualAdvances;
